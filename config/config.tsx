c9a0cf84115853638aa740c786f36b97a8f1dc04
Utkarsh <utk994@gmail.com>
2022-03-08 00:30:27 +0530

    change branch name

--- config/config.tsx
+++ config/config.tsx
@@ -3,7 +3,7 @@ import { testnetConfig } from "./testnetConfig";
 import { mainnetConfig } from "./mainnet";
 
 export const config =
-  process.env.NEXT_PUBLIC_GIT_REF === "main"
+  process.env.NEXT_PUBLIC_GIT_REF === "master"
     ? mainnetConfig
     : process.env.NEXT_PUBLIC_GIT_REF === "devnet"
     ? devnetConfig
