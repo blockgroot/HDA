70f035f4dd2a256b09c2e663d1f5f2d8eeaddca7
Amal Altibaev <maroon775@gmail.com>
2022-03-10 16:08:05 +0500

    https://stader.atlassian.net/browse/HED-86 - fix

--- components/Sidebar/Sidebar.tsx
+++ components/Sidebar/Sidebar.tsx
@@ -2,10 +2,9 @@ import { FC, useEffect } from "react";
 import c from "classnames";
 import CloseIcon from "@material-ui/icons/Close";
 
-import NavItem, { Route } from "./NavItem";
+import NavItem from "./NavItem";
 import styles from "./Sidebar.module.scss";
 import Help from "../common/Help";
-import Link from "next/link";
 import { routes } from "@constants/routes";
 
 interface Props {
@@ -31,7 +30,7 @@ const Sidebar: FC<Props> = ({
         ))}
       </div>
       <div className={styles.ovl} onClick={() => onToggleSidebar(!hide)} />
-      <div style={{ flex: 1 }}></div>
+      <div style={{ flex: 1 }}/>
       <Help className={styles.helpBtn} />
     </div>
   );
