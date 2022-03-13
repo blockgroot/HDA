f0d495561a1b4bdc1048761e77d455427863d389
Utkarsh <utk994@gmail.com>
2022-03-13 10:46:18 +0530

    Replace master with main

--- config/config.tsx
+++ config/config.tsx
@@ -3,7 +3,7 @@ import { testnetConfig } from "./testnetConfig";
 import { mainnetConfig } from "./mainnet";
 
 export const config =
-  process.env.NEXT_PUBLIC_GIT_REF === "master"
+  process.env.NEXT_PUBLIC_GIT_REF === "main"
     ? mainnetConfig
     : process.env.NEXT_PUBLIC_GIT_REF === "devnet"
     ? devnetConfig
