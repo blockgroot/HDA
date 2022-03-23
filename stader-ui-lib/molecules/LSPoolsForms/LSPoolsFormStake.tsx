ec3ceab875b1c9f8bb0f4e8cfda9babadfc03d4b
Rahul Pramod Jaguste <rahuljaguste@gmail.com>
2022-03-24 00:00:51 +0530

    fix decimals

--- stader-ui-lib/molecules/LSPoolsForms/LSPoolsFormStake.tsx
+++ stader-ui-lib/molecules/LSPoolsForms/LSPoolsFormStake.tsx
@@ -23,7 +23,7 @@ type Props = {
   walletBalance: number;
   minimumDeposit: number;
   maximumDeposit: number;
-  ustWalletBalance: number;
+  transactionFees: number;
   handleStake: (amount: number) => void;
 };
 
@@ -33,7 +33,7 @@ function LSPoolsFormStake(props: Props) {
     walletBalance,
     minimumDeposit,
     maximumDeposit,
-    ustWalletBalance,
+    transactionFees,
     handleStake,
   } = props;
   // console.log(tvlExchangeRate);
@@ -56,7 +56,10 @@ function LSPoolsFormStake(props: Props) {
       .required(
         `Deposit amount should be more than ${maximumDeposit} ${NATIVE_TOKEN_LABEL}`
       ),
-    // ust: Yup.number().moreThan(0.0, "Not enough ust for transaction fees"),
+    fees: Yup.number().moreThan(
+      0.9,
+      `Not enough Hbar for transaction fees ${transactionFees}`
+    ),
   });
 
   return (
@@ -65,7 +68,7 @@ function LSPoolsFormStake(props: Props) {
         initialValues={{
           nativeToken: 0,
           liquidNativeToken: 0,
-          ust: ustWalletBalance,
+          fees: transactionFees,
         }}
         onSubmit={(values) => {
           if (values.nativeToken) {
@@ -157,7 +160,7 @@ function LSPoolsFormStake(props: Props) {
                   total={walletBalance}
                   activeValue={nativeTokenProps.value}
                   onClick={(value) => {
-                    let val = walletBalance * value;
+                    let val = (walletBalance * value) / NATIVE_TOKEN_MULTIPLIER;
                     setFieldValue(
                       "liquidNativeToken",
                       (Number(val) * tvlExchangeRate).toFixed(precision)
@@ -167,20 +170,20 @@ function LSPoolsFormStake(props: Props) {
                   }}
                 />
                 <Typography variant={"body3"} color={"textSecondary"}>
-                  Transaction Fee: Approx 1 {NATIVE_TOKEN_LABEL}
+                  Transaction Fee: Approx {transactionFees} {NATIVE_TOKEN_LABEL}
                 </Typography>
               </div>
-              {(errors.ust || errors.nativeToken) && (
+              {(errors.fees || errors.nativeToken) && (
                 <Typography
                   variant={"body2"}
-                  color={"textSecondary"}
+                  color={"primary"}
                   fontWeight={"medium"}
-                  className={"block mt-3 text-center"}
+                  className={"block mt-8 text-center"}
                 >
-                  {errors.ust || errors.nativeToken}
+                  {errors.fees || errors.nativeToken}
                 </Typography>
               )}
-              <div className="mt-6 lg:mt-12 flex justify-center">
+              <div className="mt-4 lg:mt-8 flex justify-center">
                 <ButtonOutlined
                   className="w-[200px] h-[48px]"
                   disabled={!!Object.keys(errors).length || !values.nativeToken}
