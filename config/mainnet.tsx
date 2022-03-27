f77d871f13915dec7dea8b33d92159a4e2f53fad
Rahul Pramod Jaguste <rahuljaguste@gmail.com>
2022-03-27 16:26:30 +0530

    update url for mainnet

--- config/mainnet.tsx
+++ config/mainnet.tsx
@@ -4,7 +4,7 @@ export const mainnetConfig = {
     url: "https://mainnet-public.mirrornode.hedera.com/",
   },
   stakeApi:
-    "https://sjbw7qqdz3.execute-api.us-east-2.amazonaws.com/Prod/signTransaction",
+    "https://aa60gklj3i.execute-api.us-east-2.amazonaws.com/Prod/signTransaction",
   ids: {
     tokenId: "0.0.803264",
     stakingContractId: "0.0.803295",
