193df9f1df4402766cd01b5cbe0b9446de22cfcf
Rahul Pramod Jaguste <rahuljaguste@gmail.com>
2022-04-05 21:22:43 +0530

    commit beta branch

--- config/config.tsx
+++ config/config.tsx
@@ -3,7 +3,8 @@ import { testnetConfig } from "./testnetConfig";
 import { mainnetConfig } from "./mainnet";
 
 export const config =
-  process.env.NEXT_PUBLIC_GIT_REF === "main"
+  process.env.NEXT_PUBLIC_GIT_REF === "main" ||
+  process.env.NEXT_PUBLIC_GIT_REF === "beta"
     ? mainnetConfig
     : process.env.NEXT_PUBLIC_GIT_REF === "preview"
     ? previewNetConfig
