bee4036931e3d6c59fdbe85b394690424e86684e
Rahul Pramod Jaguste <rahuljaguste@gmail.com>
2022-04-08 14:00:56 +0530

    logo changes

--- context/HashConnectProvider.tsx
+++ context/HashConnectProvider.tsx
@@ -90,7 +90,7 @@ let APP_CONFIG: HashConnectTypes.AppMetadata = {
   name: "Stader HBAR staking",
   description:
     "Liquid staking with Stader. Stake HBAR with Stader to earn rewards while keeping full control of your staked tokens. Start earning rewards in just a few clicks",
-  icon: "https://hedera.staderlabs.com/hbarx.png",
+  icon: "https://hedera.staderlabs.com/static/stader_logo.svg",
 };
 
 const SAVE_KEY = `hashConnectData/${config.network.name}`;
@@ -547,7 +547,7 @@ const defaultProps: Partial<PropsType> = {
     name: "Stader HBAR staking",
     description:
       "Liquid staking with Stader. Stake HBAR with Stader to earn rewards while keeping full control of your staked tokens. Start earning rewards in just a few clicks",
-    icon: "https://hedera.staderlabs.com/hbarx.png",
+    icon: "https://hedera.staderlabs.com/static/stader_logo.svg",
   },
   network: config.network.name as Networks,
   debug: false,
